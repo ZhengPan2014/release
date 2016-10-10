f4efc148af0162e8305d60d48507c176b93e6a04
ouiyeah <ouiyeah@qq.com>
2016-10-10 13:27:22 +0800

    test

--- boot.sh
+++ boot.sh
@@ -2,9 +2,9 @@ sleep 10s
 
 source ~/catkin_ws/base.sh
 
-if [ -f "/etc/udev/rules.d/70-persistent-net.rules" ]; then
-    sudo rm /etc/udev/rules.d/70-persistent-net.rules
-fi
-sudo ifconfig eth0 $ROS_USER_IP netmask 255.255.255.0 up
+#if [ -f "/etc/udev/rules.d/70-persistent-net.rules" ]; then
+#    sudo rm /etc/udev/rules.d/70-persistent-net.rules
+#fi
+#sudo ifconfig eth0 $ROS_USER_IP netmask 255.255.255.0 up
 
 roslaunch bringup bringup-boot.launch
