741f29bf3e62ac768becbc87fcd1fdfe906ee3b9
ouiyeah <ouiyeah@qq.com>
2016-10-30 22:04:37 +0800

    relink base/boot.sh

--- base.sh
+++ base.sh
@@ -1,15 +1,1 @@
-source /opt/ros/indigo/setup.bash
-source ~/catkin_ws/devel/setup.bash
-
-ROS_USER_MODEL=`hostname | awk -F '-' '{print $2}'`
-if [ -d "$HOME/catkin_ws/devel/share/bringup/launch/$ROS_USER_MODEL" ]; then
-    export ROS_USER_MODEL=$ROS_USER_MODEL
-else
-    if [ -d "$HOME/catkin_ws/src/bringup/launch/$ROS_USER_MODEL" ]; then
-        export ROS_USER_MODEL=$ROS_USER_MODEL
-    else
-        unset ROS_USER_MODEL
-    fi
-fi
-
-export ROS_USER_IP=`hostname -I | awk -F '.' '{print $1"."$2"."$3".7"}'`
+install/share/bringup/shell/base.sh
--- boot.sh
+++ boot.sh
@@ -1,10 +1,1 @@
-sleep 10s
-
-source ~/catkin_ws/base.sh
-
-#if [ -f "/etc/udev/rules.d/70-persistent-net.rules" ]; then
-#    sudo rm /etc/udev/rules.d/70-persistent-net.rules
-#fi
-#sudo ifconfig eth0 $ROS_USER_IP netmask 255.255.255.0 up
-
-roslaunch bringup bringup-boot.launch
+install/share/bringup/shell/boot.sh
