d9308ab8ea22504459c7d52a47b75300534823bc
ouiyeah <ouiyeah@qq.com>
2016-11-14 15:52:46 +0800

    update map and ster parameters

--- install/share/bringup/shell/save_map_edit.sh
+++ install/share/bringup/shell/save_map_edit.sh
@@ -1,5 +1,5 @@
 #roslaunch bringup map_edit_saver.launch;
 source ~/catkin_ws/base.sh;
 roscd bringup/param/;
-rosrun map_server map_saver map:=map_edit;
+rosrun map_server map_saver -f map_edit;
 rostopic pub -1 /shell_feedback std_msgs/String "save_map_edit";
