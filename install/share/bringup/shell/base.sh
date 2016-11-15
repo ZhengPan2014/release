2628af31396108c58a5711c5a718a73f941893d9
ouiyeah <ouiyeah@qq.com>
2016-11-15 15:40:00 +0800

    add user_model.sh

--- install/share/bringup/shell/base.sh
+++ install/share/bringup/shell/base.sh
@@ -1,15 +1,12 @@
-source /opt/ros/indigo/setup.bash
-source ~/catkin_ws/devel/setup.bash
+source /opt/ros/indigo/setup.bash;
+source ~/catkin_ws/devel/setup.bash;
 
-ROS_USER_MODEL=`hostname | awk -F '-' '{print $2}'`
-if [ -d "$HOME/catkin_ws/devel/share/bringup/launch/$ROS_USER_MODEL" ]; then
+ROS_USER_MODEL=`hostname | awk -F '-' '{print $2}'`;
+roscd bringup;
+if [ -d "launch/$ROS_USER_MODEL" ]; then
     export ROS_USER_MODEL=$ROS_USER_MODEL
 else
-    if [ -d "$HOME/catkin_ws/src/bringup/launch/$ROS_USER_MODEL" ]; then
-        export ROS_USER_MODEL=$ROS_USER_MODEL
-    else
-        unset ROS_USER_MODEL
-    fi
+    unset ROS_USER_MODEL
 fi
 
 export ROS_USER_IP=`hostname -I | awk -F '.' '{print $1"."$2"."$3".7"}'`
--- install/share/bringup/shell/version.sh
+++ install/share/bringup/shell/version.sh
@@ -1,5 +1,5 @@
-_RC=`grep "|" ~/catkin_ws/README.md`;
-_FB=`echo $_RC | awk -F '|' '{print $10}'`;
-_FB=`echo version:$_FB | awk -F ' ' '{print $1$2}'`;
+ROS_HISTORY=`grep "|" ~/catkin_ws/README.md`;
+ROS_VERSION=`echo $ROS_HISTORY | awk -F '|' '{print $10}'`;
+_FB=`echo version:$ROS_VERSION | awk -F ' ' '{print $1$2}'`;
 rostopic pub -1 /shell_feedback std_msgs/String $_FB;
-unset _FB; unset _RC;
+unset _FB;
