bffe6a51902b7d80a2562f7d0fa2d844c1a9ed1c
ouiyeah <ouiyeah@qq.com>
2017-01-03 14:59:40 +0800

    change a map_saver launch bug

--- install/share/bringup/shell/save_as_map.sh
+++ install/share/bringup/shell/save_as_map.sh
@@ -1,5 +1,6 @@
-#roslaunch bringup map_as_saver.launch;
 source ~/catkin_ws/base.sh;
 roscd bringup/param/;
-rosrun map_server map_saver map:=map_as;
+#rosrun map_server map_saver map:=map_as;
+roslaunch bringup map_as_saver.launch;
+sed -i -e "s/image:.*/image: map.pgm/" map.yaml;
 rostopic pub -1 /shell_feedback std_msgs/String "save_as_map";
--- install/share/bringup/shell/save_map.sh
+++ install/share/bringup/shell/save_map.sh
@@ -1,5 +1,6 @@
-#roslaunch bringup map_saver.launch;
 source ~/catkin_ws/base.sh;
 roscd bringup/param/;
-rosrun map_server map_saver;
+#rosrun map_server map_saver;
+roslaunch bringup map_saver.launch;
+sed -i -e "s/image:.*/image: map.pgm/" map.yaml;
 rostopic pub -1 /shell_feedback std_msgs/String "save_map";
