5a711bf5fed86103c241139b1e1977c79b4400a0
ouiyeah <ouiyeah@qq.com>
2017-01-05 15:49:56 +0800

    important update

--- install/share/bringup/shell/update.sh
+++ install/share/bringup/shell/update.sh
@@ -1,9 +1,9 @@
 cd ~/workspaces/hitrobot/release;
-rostopic pub -1 /shell_feedback std_msgs/String "software_update `git pull`";
+rostopic pub -1 /shell_feedback std_msgs/String "update `git pull`";
 _RC=$?;
 if [ $_RC -eq 0 ]; then
-    rostopic pub -1 /shell_feedback std_msgs/String "software_update success";
+    rostopic pub -1 /shell_feedback std_msgs/String "update success";
 else
-    rostopic pub -1 /shell_feedback std_msgs/String "software_update failure";
+    rostopic pub -1 /shell_feedback std_msgs/String "update failure";
 fi;
 unset _RC;
