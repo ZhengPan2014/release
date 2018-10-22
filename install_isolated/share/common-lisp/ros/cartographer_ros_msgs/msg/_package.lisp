2400119a319eecb5fbb39cdb9473cdbbe070bed6
ouiyeah-titan <ouiyeah@qq.com>
2018-10-22 21:45:18 +0800

    add cartographer install

---
+++ install_isolated/share/common-lisp/ros/cartographer_ros_msgs/msg/_package.lisp
@@ -0,0 +1,23 @@
+(cl:defpackage cartographer_ros_msgs-msg
+  (:use )
+  (:export
+   "<SUBMAPENTRY>"
+   "SUBMAPENTRY"
+   "<TRAJECTORYOPTIONS>"
+   "TRAJECTORYOPTIONS"
+   "<SUBMAPLIST>"
+   "SUBMAPLIST"
+   "<STATUSCODE>"
+   "STATUSCODE"
+   "<STATUSRESPONSE>"
+   "STATUSRESPONSE"
+   "<LANDMARKENTRY>"
+   "LANDMARKENTRY"
+   "<SENSORTOPICS>"
+   "SENSORTOPICS"
+   "<LANDMARKLIST>"
+   "LANDMARKLIST"
+   "<SUBMAPTEXTURE>"
+   "SUBMAPTEXTURE"
+  ))
+
