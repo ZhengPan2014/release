01a86436d8818e429014345a801ed4b2717adfd5
ouiyeah-titan <ouiyeah@qq.com>
2019-02-19 15:09:53 +0800

    update dwa_local for footprint_spec

--- install/include/dwa_local_planner/dwa_planner.h
+++ install/include/dwa_local_planner/dwa_planner.h
@@ -85,7 +85,7 @@ namespace dwa_local_planner {
       /**
        * @brief Reconfigures the trajectory planner
        */
-      void reconfigure(DWAPlannerConfig &cfg, const std::vector<geometry_msgs::Point> &footprint_spec);
+      void reconfigure(DWAPlannerConfig &cfg);
 
       /**
        * @brief  Check if a trajectory is legal for a position/velocity pair
@@ -113,11 +113,19 @@ namespace dwa_local_planner {
           std::vector<geometry_msgs::Point> footprint_spec);
 
       /**
-       * @brief  Take in a new global plan for the local planner to follow, and adjust local costmaps
+       * @brief  Update the cost functions before planning
+       * @param  global_pose The robot's current pose
        * @param  new_plan The new global plan
+       * @param  footprint_spec The robot's footprint
+       *
+       * The obstacle cost function gets the footprint.
+       * The path and goal cost functions get the global_plan
+       * The alignment cost functions get a version of the global plan
+       *   that is modified based on the global_pose 
        */
       void updatePlanAndLocalCosts(tf::Stamped<tf::Pose> global_pose,
-          const std::vector<geometry_msgs::PoseStamped>& new_plan);
+          const std::vector<geometry_msgs::PoseStamped>& new_plan,
+          const std::vector<geometry_msgs::Point>& footprint_spec);
 
       /**
        * @brief Get the period at which the local planner is expected to run
