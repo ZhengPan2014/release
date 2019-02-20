3e9f638d23346b30a1b25bddbb4d67fb28377f52
ouiyeah-titan <ouiyeah@qq.com>
2019-02-20 13:33:30 +0800

    add obstacle_detector

---
+++ install/share/common-lisp/ros/obstacle_detector/msg/_package.lisp
@@ -0,0 +1,11 @@
+(cl:defpackage obstacle_detector-msg
+  (:use )
+  (:export
+   "<CIRCLEOBSTACLE>"
+   "CIRCLEOBSTACLE"
+   "<SEGMENTOBSTACLE>"
+   "SEGMENTOBSTACLE"
+   "<OBSTACLES>"
+   "OBSTACLES"
+  ))
+
